70b1b54aa77b84587622decdd8f94f8fb1a85d30
stu2301321035 <stu2301321035@uni-plovdiv.bg>
2025-11-18 14:32:43 +0200

    Implementing AddEditActivity.java and creating edit_shape.xml for background of the edit texts

--- app/src/main/java/com/example/mynotesapp/MainActivity.java
+++ app/src/main/java/com/example/mynotesapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mynotesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton floatingId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        floatingId = findViewById(R.id.floatingId);
+        floatingId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddEditActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }

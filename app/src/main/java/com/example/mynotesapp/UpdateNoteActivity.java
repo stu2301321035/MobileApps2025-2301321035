dce97f6afb5ded8c67f4b7c7051cd54c23cc1210
stu2301321035 <stu2301321035@uni-plovdiv.bg>
2025-11-18 18:01:04 +0200

    Add UpdateNoteActivity.java in AndroidManifest.xml

--- app/src/main/java/com/example/mynotesapp/UpdateNoteActivity.java
+++ app/src/main/java/com/example/mynotesapp/UpdateNoteActivity.java
@@ -1,6 +1,8 @@
 package com.example.mynotesapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class UpdateNoteActivity extends AppCompatActivity {
 
+    EditText updateTitle, updateDesc;
+    Button btnUpdate;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_update_note);
+
+        updateTitle = findViewById(R.id.edUpdateTitle);
+        updateDesc = findViewById(R.id.edUpdateDesc);
+        btnUpdate = findViewById(R.id.btnUpdate);
+
+
 
 
     }
--- app/src/main/java/com/example/mynotesapp/adapter/NoteAdapter.java
+++ app/src/main/java/com/example/mynotesapp/adapter/NoteAdapter.java
@@ -1,16 +1,20 @@
 package com.example.mynotesapp.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mynotesapp.MainActivity;
 import com.example.mynotesapp.R;
+import com.example.mynotesapp.UpdateNoteActivity;
 import com.example.mynotesapp.model.Note;
 
 import java.util.ArrayList;
@@ -35,9 +39,23 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NoteAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NoteAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.tvTitle.setText(arrayList.get(position).getTitle());
         holder.tcDesc.setText(arrayList.get(position).getDescription());
+
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, UpdateNoteActivity.class);
+
+                intent.putExtra("id", arrayList.get(position).getId());
+                intent.putExtra("title", arrayList.get(position).getTitle());
+                intent.putExtra("description", arrayList.get(position).getDescription());
+
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -48,11 +66,13 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
     public  class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView tvTitle, tcDesc;
+        CardView cardView;
         public ViewHolder(@NonNull View itemView){
             super(itemView);
 
             tcDesc = itemView.findViewById(R.id.tvDesc); // Use tvDesc
             tvTitle = itemView.findViewById(R.id.tvTitle); // Use tvTitle
+            cardView = itemView.findViewById(R.id.cardView);
 
         }
     }

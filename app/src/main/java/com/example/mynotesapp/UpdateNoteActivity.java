6bb58e61afbd59fe3492f5a55fca3e5919fb140d
stu2301321035 <stu2301321035@uni-plovdiv.bg>
2025-11-18 19:49:17 +0200

    Implementing UpdateNoteActivity.java

--- app/src/main/java/com/example/mynotesapp/UpdateNoteActivity.java
+++ app/src/main/java/com/example/mynotesapp/UpdateNoteActivity.java
@@ -1,14 +1,22 @@
 package com.example.mynotesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mynotesapp.database.NoteHelper;
+
+import org.jetbrains.annotations.TestOnly;
 
 public class UpdateNoteActivity extends AppCompatActivity {
 
@@ -25,6 +33,31 @@ public class UpdateNoteActivity extends AppCompatActivity {
         updateTitle = findViewById(R.id.edUpdateTitle);
         updateDesc = findViewById(R.id.edUpdateDesc);
         btnUpdate = findViewById(R.id.btnUpdate);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //get the data from the MainActivity
+        String title = getIntent().getStringExtra("title");
+        String desc = getIntent().getStringExtra("description");
+        int id = getIntent().getIntExtra("id",0);
+        updateTitle.setText(title);
+        updateDesc.setText(desc);
+        String sId = String.valueOf(id);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (updateTitle.length()>0&&updateDesc.length()>0){
+                    NoteHelper noteHelper = new NoteHelper(UpdateNoteActivity.this);
+                    noteHelper.updateData(updateTitle.getText().toString(), updateDesc.getText().toString(),sId);
+                    Toast.makeText(UpdateNoteActivity.this, "The Data is Successfully Updated", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(UpdateNoteActivity.this,MainActivity.class));
+                }else{
+                    Toast.makeText(UpdateNoteActivity.this, "The Data is not Updated", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
